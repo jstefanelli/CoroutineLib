d0bc6dc855a704a6d954683b200fa05d668898ff
John Stefanelli <jpsdzn@gmail.com>
2023-02-09 11:31:35 +0100

    [CoroutineLib] Add boundless queue implementation

---
+++ CoroutineLib/include/crlib/cc_boundless_queue.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <memory>
+#include <optional>
+#include <atomic>
+
+namespace crlib {
+	template<typename T>
+	struct BoundlessQueueItem;
+
+	template<typename T>
+	struct BoundlessQueueItem {
+		using ptr_type = std::shared_ptr<BoundlessQueueItem<T>>;
+		using atomic_ptr_t = std::atomic<ptr_type>;
+		std::optional<T> value;
+		atomic_ptr_t next;
+
+		BoundlessQueueItem() : value(std::nullopt), next(nullptr) {
+
+		}
+
+		BoundlessQueueItem(const BoundlessQueueItem<T>&) = delete;
+		BoundlessQueueItem& operator=(const BoundlessQueueItem<T>&) = delete;
+	};
+
+	template<typename T>
+	struct BoundlessQueue {
+		BoundlessQueueItem<T>::atomic_ptr_t head;
+		BoundlessQueueItem<T>::atomic_ptr_t tail;
+
+		BoundlessQueue() {
+			auto item = std::make_shared<BoundlessQueueItem<T>>();
+			item->value = std::nullopt;
+			item->next.store(nullptr);
+
+			head.store(item);
+			tail.store(item);
+		}
+
+		BoundlessQueue(const BoundlessQueue<T>&) = delete;
+		BoundlessQueue& operator=(const BoundlessQueue<T>&) = delete;
+
+		void push(const T& val) {
+			auto item = std::make_shared<BoundlessQueueItem<T>>();
+			item->value = val;
+			item->next.store(nullptr);
+
+			bool ok;
+			typename BoundlessQueueItem<T>::ptr_type p;
+			typename BoundlessQueueItem<T>::ptr_type c;
+			typename BoundlessQueueItem<T>::ptr_type next;
+			do {
+				c = nullptr;
+				p = tail.load();
+				do {
+					next = p->next.load();
+					if (next != nullptr) {
+						p = next;
+					}
+				} while(next != nullptr);
+				ok = p->next.compare_exchange_weak(c, item);
+			} while(!ok);
+
+			tail.compare_exchange_weak(p, item);
+		}
+
+		std::optional<T> pull() {
+			typename BoundlessQueueItem<T>::ptr_type p, next;
+			do {
+				p = head.load();
+				next = p->next.load();
+				if (next == nullptr) {
+					return std::nullopt;
+				}
+			} while(!head.compare_exchange_weak(p, next));
+
+			auto v = next->value;
+			next->value = std::nullopt;
+
+			return v;
+		}
+	};
+}
--- CoroutineTest/QueueTest.cpp
+++ CoroutineTest/QueueTest.cpp
@@ -1,10 +1,80 @@
 #include <thread>
 #include <iostream>
 #include <crlib/cc_queue_2.h>
+#include <crlib/cc_boundless_queue.h>
 #include <memory>
 #include <string>
 #include <set>
 #include <mutex>
+
+bool test_boundless() {
+	std::cout << "[QueueTest] Running boundless queue test" << std::endl;
+
+	auto q = std::make_shared<crlib::BoundlessQueue<int>>();
+	std::vector<std::thread> writers;
+
+	for(int i = 0; i < 10; i++) {
+		writers.emplace_back([i, q]() {
+			for (int j = 0; j < 1000; j++) {
+				int v = (j * 10) + i;
+				q->push(v);
+			}
+		});
+	}
+
+	for(auto& t : writers) {
+		t.join();
+	}
+
+
+	std::vector<std::thread> readers;
+
+	std::mutex lock;
+	std::vector<std::set<int>> results;
+	for(int i = 0; i < 10; i++) {
+		readers.emplace_back([i, q, &lock, &results]() {
+			std::set<int> rs;
+
+			std::optional<int> val;
+			do {
+				val = q->pull();
+
+				if (val.has_value()) {
+					rs.insert(val.value());
+				}
+			} while(val.has_value());
+
+			{
+				auto l = std::lock_guard(lock);
+
+				results.push_back(rs);
+			}
+		});
+	}
+
+
+	for(auto& t : readers) {
+		t.join();
+	}
+
+	bool anyError = false;
+	for (int i = 0; i < 10000; i++) {
+		bool found = false;
+		for(auto& set : results) {
+			if (set.contains(i)) {
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			std::cout << "[QueueTest] Missing value: " << i << std::endl;
+			anyError = true;
+		}
+	}
+
+	return !anyError;
+}
 
 bool test_generic() {
 	std::cout << "[QueueTest] Running generic queue test" << std::endl;
@@ -146,5 +216,6 @@ int main(int argc, char** argv) {
 		return test_readOnce() ? 0 : 1;
 	}
 
-	return test_generic() ? 0 : 1;
+	//return test_generic() ? 0 : 1;
+	return test_boundless() ? 0 : 1;
 }
